116e75046d1f1a382b54affd7f91bbfb5706e7f6
CondorPalenski <Mikh96@yandex.ru>
2018-04-14 22:36:03 +0300

    Изменил название и заголов формы

--- NewCalculatorProj/MainForm.Designer.cs
+++ NewCalculatorProj/MainForm.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace NewCalculatorProj
 {
-    partial class Form1
+    partial class MainForm
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -30,13 +30,13 @@
         {
             this.SuspendLayout();
             // 
-            // Form1
+            // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(560, 450);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Name = "MainForm";
+            this.Text = "Calculator";
             this.ResumeLayout(false);
 
         }
